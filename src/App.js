fa385062a21ccec296bdc0c33e27bdd88f0ee423
jaychang99 <jaychang99@gmail.com>
2022-12-28 12:05:24 +0900

    feat: App.js cleanup 작업

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
 import "./App.css";
-import { useState } from "react";
-import UserCard from "./common/UserCard";
-import UserCardSquashed from "./common/UserCardSquashed";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        증가
-      </button>
-      <h1>{count}</h1>
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        감소
-      </button>
-      <hr />
-      <UserCard />
-      <UserCardSquashed />
-    </>
-  );
+  return <div>UseRef 스터디</div>;
 }
 
 export default App;
