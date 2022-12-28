b2779b5016b3d812edecdd5a1e655fc05b4143f5
jaychang99 <jaychang99@gmail.com>
2022-12-28 20:26:20 +0900

    feat: 신용카드 예시 로직 구현 완료

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import UseRefStudy from "common/UseRefStudy";
 import UseRefStudyFocus from "common/UseRefStudyFocus";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import UseRefCardExample from "common/UseRefCardExample";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" exact={true} element={<UseRefStudy />} />
         <Route path="/focus" exact={true} element={<UseRefStudyFocus />} />
+        <Route path="/card" exact={true} element={<UseRefCardExample />} />
       </Routes>
     </Router>
   );
