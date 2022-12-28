98567579a7a9f8715d81db2e3578c44e93938c34
jaychang99 <jaychang99@gmail.com>
2022-12-28 16:54:01 +0900

    refactor: react-router 이용해서 페이지 분리

--- src/App.js
+++ src/App.js
@@ -1,88 +1,14 @@
-import { useRef, useState } from "react";
+import UseRefStudy from "common/UseRefStudy";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  let myVar = 0;
-  const [myState, setMyState] = useState(0);
-  const myRef = useRef(0);
-
-  // 아무것도 안하고 그냥 rerender 만 시키기
-  const [rerender, setRerender] = useState(true);
-
-  function triggerRerender() {
-    // state 가 바뀌면 rerender 된다는 점을 이용.
-    // rerender 변수가 true <-> false 바뀌면서 계속 trigger (값 자체는 중요치 않음)
-    setRerender(!rerender);
-  }
-
-  //출력 관련
-  function displayValues() {
-    console.log(`myVar: ${myVar}, myState: ${myState}, myRef: ${myRef.current}`);
-  }
-
-  // myVar 관련
-
-  function incrementMyVar() {
-    myVar += 1;
-    displayValues();
-  }
-
-  function decrementMyVar() {
-    myVar -= 1;
-    displayValues();
-  }
-
-  // myState 관련
-
-  function incrementMyState() {
-    setMyState(myState + 1);
-    displayValues();
-  }
-
-  function decrementMyState() {
-    setMyState(myState - 1);
-    displayValues();
-  }
-
-  // myRef 관련
-
-  function incrementMyRef() {
-    myRef.current += 1;
-    displayValues();
-  }
-
-  function decrementMyRef() {
-    myRef.current -= 1;
-    displayValues();
-  }
-
   return (
-    <div>
-      <h1>UseRef 스터디</h1>
-      <div className="container">
-        <div className="box">
-          <h2>일반 변수 사용</h2>
-          <p>myVar 의 값: {myVar}</p>
-          <button onClick={incrementMyVar}>myVar 증가시키기</button>
-          <button onClick={decrementMyVar}>myVar 감소시키기</button>
-        </div>
-        <div className="box">
-          <h2>state 사용</h2>
-          <p>myState 의 값: {myState}</p>
-          <button onClick={incrementMyState}>myState 증가시키기</button>
-          <button onClick={decrementMyState}>myState 감소시키기</button>
-        </div>
-        <div className="box">
-          <h2>ref 사용</h2>
-          <p>myRef.current 의 값: {myRef.current}</p>
-          <button onClick={incrementMyRef}>myRef 증가시키기</button>
-          <button onClick={decrementMyRef}>myRef 감소시키기</button>
-        </div>
-        <div className="box">
-          <button onClick={triggerRerender}>아무것도 하지 않고 그냥 rerender 만 시키기!</button>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact={true} element={<UseRefStudy />} />
+      </Routes>
+    </Router>
   );
 }
 
