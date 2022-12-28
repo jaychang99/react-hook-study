53fa78b944377b2f5fccfadb1c7cc9860758fd80
jaychang99 <jaychang99@gmail.com>
2022-12-28 12:15:45 +0900

    feat: 기본적인 변수사용, state 사용 ref 사용 의 UI 작성 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>UseRef 스터디</div>;
+  let myVar = 0;
+  const [myState, setMyState] = useState(0);
+  const myRef = useRef(0);
+
+  return (
+    <div>
+      <h1>UseRef 스터디</h1>
+      <div class="container">
+        <div class="box">
+          <h2>일반 변수 사용</h2>
+          <p>myVar 의 값: {myVar}</p>
+        </div>
+        <div class="box">
+          <h2>state 사용</h2>
+          <p>myState 의 값: {myState}</p>
+        </div>
+        <div class="box">
+          <h2>ref 사용</h2>
+          <p>myRef.current 의 값: {myRef.current}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
