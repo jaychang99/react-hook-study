c906f238c65b0adb34c286e81e2d60f511e39f58
jaychang99 <jaychang99@gmail.com>
2022-12-28 20:30:38 +0900

    blanked: UseRefCardExample.js

--- src/common/UseRefCardExample.js
+++ src/common/UseRefCardExample.js
@@ -1,62 +1,7 @@
 import "common/styles/useRefCardExample.css";
-import { useRef } from "react";
 
 function UseRefCardExample() {
-  const cardNo1Ref = useRef();
-  const cardNo2Ref = useRef();
-  const cardNo3Ref = useRef();
-  const cardNo4Ref = useRef();
-
-  const submitButtonRef = useRef();
-
-  function handleInputChange(e, nextRef) {
-    const currentInputValue = e.target.value;
-    if (nextRef) {
-      if (currentInputValue.length === 4) {
-        nextRef.current.focus();
-      }
-    }
-  }
-
-  return (
-    <div>
-      <h1>카드번호</h1>
-      <input
-        ref={cardNo1Ref}
-        onChange={(e) => {
-          handleInputChange(e, cardNo2Ref);
-        }}
-      />
-      <input
-        ref={cardNo2Ref}
-        onChange={(e) => {
-          handleInputChange(e, cardNo3Ref);
-        }}
-      />
-      <input
-        ref={cardNo3Ref}
-        onChange={(e) => {
-          handleInputChange(e, cardNo4Ref);
-        }}
-      />
-      <input
-        ref={cardNo4Ref}
-        onChange={(e) => {
-          handleInputChange(e, submitButtonRef);
-        }}
-      />
-      <button
-        ref={submitButtonRef}
-        onClick={() => {
-          console.log(
-            `신용카드 번호는 ${cardNo1Ref.current.value}-${cardNo2Ref.current.value}-${cardNo3Ref.current.value}-${cardNo4Ref.current.value}`
-          );
-        }}
-      >
-        제출
-      </button>
-    </div>
-  );
+  return <div>여기에 카드 입력을 만들어 보아요</div>;
 }
 
 export default UseRefCardExample;
