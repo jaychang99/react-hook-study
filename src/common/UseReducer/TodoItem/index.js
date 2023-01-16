399febac9d4011fc53c87a6ed7d54e64c97a6b82
jaychang99 <jaychang99@gmail.com>
2023-01-16 14:46:58 +0900

    feat: useReducer 로 할 일 toggle 구현

--- src/common/UseReducer/TodoItem/index.js
+++ src/common/UseReducer/TodoItem/index.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
-function TodoItem({ name, dispatch, id }) {
+function TodoItem({ name, dispatch, id, completed }) {
   return (
     <div>
-      <span>
+      <span onClick={() => {
+        dispatch({ type: "toggle-todo", id: id })
+      }} style={{
+
+        color: completed ? "gray" : "black",
+        textDecoration: completed ? "line-through" : "none",
+        fontWeight: completed ? "400" : "700"
+      }
+      }>
         {name}
 
       </span>
       <button onClick={() => {
         dispatch({ type: "delete-todo", id: id })
       }}>삭제</button>
-    </div>
+    </div >
   )
 }
 
--- src/common/UseReducer/index.js
+++ src/common/UseReducer/index.js
@@ -1,9 +1,10 @@
 import TodoItem from 'common/UseReducer/TodoItem'
-import { useReducer, useState } from 'react'
+import { useReducer, useState, useRef } from 'react'
 
 
 const initialState = {
   count: 0,
+  incomplete: 0,
   items: [
 
   ]
@@ -15,17 +16,34 @@ function reducer(state, action) {
 
       const newItem = {
         id: Date.now(),
-        name: action.name
+        name: action.name,
+        completed: false,
       }
       return {
         count: state.count + 1,
+        incomplete: state.incomplete + 1,
         items: [...state.items, newItem]
+      }
+    case "toggle-todo":
+
+
+      return {
+        count: state.count,
+        incomplete: state.items.filter((item) => item.id === action.id)[0].completed ? state.incomplete + 1 : state.incomplete - 1,
+        items: state.items.map((item) => {
+          if (item.id === action.id) {
+            return { ...item, completed: !item.completed }
+          } else {
+            return item
+          }
+        })
       }
     case "delete-todo":
 
 
       return {
         count: state.count - 1,
+        incomplete: state.items.filter((item) => item.id === action.id)[0].completed ? state.incomplete : state.incomplete - 1,
         items: state.items.filter((item) => item.id !== action.id)
       }
     default:
@@ -38,21 +56,24 @@ function UseReducerPage() {
   const [inputValue, setInputValue] = useState("")
   const [data, dispatch] = useReducer(reducer, initialState)
 
+  const inputRef = useRef(null)
+
   return (
     <div>
       <h1>Todo List</h1>
       <div>
         <span>총 할 일 개수: {data.count}  </span>
-        <span>완료됨: 3  </span>
-        <span>미완료: 1  </span>
+        <span>완료됨: {data.count - data.incomplete}  </span>
+        <span>미완료: {data.incomplete}  </span>
       </div>
-      <input onChange={(e) => { setInputValue(e.target.value) }} />
+      <input ref={inputRef} onChange={(e) => { setInputValue(e.target.value) }} />
       <button onClick={() => {
         dispatch({ type: "add-todo", name: inputValue })
+        inputRef.current.value = ""
       }}>등록</button>
       <hr />
       <div>
-        {data.items.map((item) => (<TodoItem name={item.name} id={item.id} dispatch={dispatch} />))}
+        {data.items.map((item) => (<TodoItem name={item.name} id={item.id} dispatch={dispatch} completed={item.completed} />))}
       </div>
 
     </div>
